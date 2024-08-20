7aae2cfa1ac33cbf4c5484696b84f23eb4ac36c8
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-08-20 09:52:01 +0530

    Fix: Protected Route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,18 +1,22 @@
-import { useUser } from "@clerk/clerk-react";
 import { Navigate, useLocation } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
 
 const ProtectedRoute = ({ children }) => {
-  const { isSignedIn, user, isLoaded } = useUser();
-  const { pathName } = useLocation();
+  const { isSignedIn, isLoaded, user } = useUser();
+  const { pathname } = useLocation();
 
   if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
-    return <Navigate to="/sign-in=true" />;
+    return <Navigate to="/?sign-in=true" />;
   }
-  return (
-    <div>
-      <>Hello</>
-    </div>
-  );
+
+  if (
+    user !== undefined &&
+    !user?.unsafeMetadata?.role &&
+    pathname !== "/onboarding"
+  )
+    return <Navigate to="/onboarding" />;
+
+  return children;
 };
 
 export default ProtectedRoute;
