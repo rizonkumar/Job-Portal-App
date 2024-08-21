969376592f5871e9e8a1f67828742c0cd7a9b47b
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-08-21 06:54:17 +0530

    Feat: Added Company Logo and Name

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -1,8 +1,22 @@
 import supabaseClient from "./../utils/supabase";
 
-export async function getJobs(token) {
+export async function getJobs(token, { location, company_id, searchQuery }) {
   const supabase = await supabaseClient(token);
-  let query = supabase.from("jobs").select("*");
+  let query = supabase
+    .from("jobs")
+    .select("*, company: companies(name, logo_url)");
+
+  if (location) {
+    query = query.eq("location", location);
+  }
+
+  if (company_id) {
+    company_id = query.eq("company_id", company_id);
+  }
+
+  if (searchQuery) {
+    query = query.ilike("title", `%${searchQuery}%`);
+  }
 
   const { data, error } = await query;
 
