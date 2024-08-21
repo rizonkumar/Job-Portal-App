a0e7649fe4201c3d9f68bdaa1be66c4b3bda0354
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-08-21 06:18:32 +0530

    Feat: Calling the API in the UI

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -1,5 +1,22 @@
+import { getJobs } from "@/api/jobs";
+import { useSession } from "@clerk/clerk-react";
+import { useEffect } from "react";
+
 const JobListing = () => {
-  return <div>Landing</div>;
+  const { session } = useSession();
+
+  const fetchJobs = async () => {
+    const supabaseAccessToken = await session.getToken({
+      template: "supabase",
+    });
+    const data = await getJobs(supabaseAccessToken);
+    console.log("Data", data);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  });
+  return <div></div>;
 };
 
 export default JobListing;
