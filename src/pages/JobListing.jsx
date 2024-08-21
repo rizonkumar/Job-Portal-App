f8f122aea221818c6c6e610db048b916ccb7dafd
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-08-21 07:17:41 +0530

    Feat: Implemented Job Listing

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -41,7 +41,7 @@ const JobListing = () => {
         <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
       )}
       {loadingJobs === false && (
-        <div>
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
           {jobs.length ? (
             jobs.map((job) => {
               return <JobCard key={job.id} job={job} />;
