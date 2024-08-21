67c14eb6ee13440cea7868fad6704599b96c792c
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-08-21 07:17:09 +0530

    Feat: Added Saved Jobs

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -4,7 +4,7 @@ export async function getJobs(token, { location, company_id, searchQuery }) {
   const supabase = await supabaseClient(token);
   let query = supabase
     .from("jobs")
-    .select("*, company: companies(name, logo_url)");
+    .select("*, company: companies(name, logo_url), saved: saved_jobs(id)");
 
   if (location) {
     query = query.eq("location", location);
