1e3ccee91e2cecd66e7fd4e78e00309485f1d344
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-08-21 06:18:16 +0530

    Feat: Implemented getJobs API

---
+++ src/api/jobs.js
@@ -0,0 +1,14 @@
+import supabaseClient from "./../utils/supabase";
+
+export async function getJobs(token) {
+  const supabase = await supabaseClient(token);
+  let query = supabase.from("jobs").select("*");
+
+  const { data, error } = await query;
+
+  if (error) {
+    console.error("error fetching jobs", error);
+    return null;
+  }
+  return data;
+}
